91fd3ee4374778bc1046943a79c8de0d881fea1c
major0001 <major0001>
2022-05-29 16:39:14 +0000

    Having fun using Vi editor and Python

--- 0x00-vagrant/my-first-python.py
+++ 0x00-vagrant/my-first-python.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
-%matpltlib inline
+%matplotlib inline
 x= np.linspace(0,3,200)
 y =x*3
 plt.plot(x,y,"-g",lable="plot of x against y")
